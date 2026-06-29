fdb957b31ca9644023f9f32a02da2e4e08b4198e
Sumit Mukherjee <sumitmukherjee15@gmail.com>
2026-06-29 06:19:08 +0530

    Updated day1 code

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ response = requests.post(
     "http://localhost:11434/api/generate",
     json ={
         "model": "llama3.2:1b",
-        "prompt": "In oe sentence, explain what an api is"
-        , "stream": False
+        "prompt": "In one sentence, explain what an api is",
+        "stream": False
     }
 )
 
-print(response.json()["response"])
+result = response.json()
+print(result["response"])
